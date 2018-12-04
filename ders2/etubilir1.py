346ffc5f631e70732a7a3bd69a4c8cc37202552d
Ali İlteriş Keskin <ilteriskesk20@gmail.com>
2018-12-04 16:31:44 +0300

    Add files via upload

---
+++ ders2/etubilir1.py
@@ -0,0 +1,123 @@
+## -*- coding: utf-8 -*-
+#
+#sayi1 = 3
+#karakter1 = 'a'
+#karakter2 = '3'
+#karakter3 = ''
+#sayi2 = 2
+#
+#print('Merhaba Python')
+#
+#print('sayi1 = ', sayi1)
+#
+#print("Ali'nin arabası çok güzel.")
+#
+#print("sayi1'in tipi     : ", type(sayi1))
+#print("karakter1'in tipi : ", type(karakter1))
+#print("karakter3'nin tipi: ", type(karakter3))
+#
+#toplam = sayi1 + sayi2
+#print('Toplam = ', toplam)
+#
+#carpim = sayi1 * sayi2
+#print('Çarpım = ', carpim)
+#
+#mod = sayi1 % sayi2
+#print('Mod = ', mod)
+#
+#bolum = sayi1 / sayi2
+#print('Bölüm = ', bolum)
+
+#meyve1 = "elma"
+#meyve2 = "armut"
+#
+#sayi1 = '3'
+#sayi2 = '2'
+#
+#sayi3 = 3
+#
+#print(meyve1 + meyve2)
+#print(sayi1 + sayi2)
+#
+#sayi3 = str(sayi3)
+#print(sayi2 + sayi3)
+#
+#ondalikli1 = 3.5
+#ondalikli2 = 3.3
+#ondalikli3 = '4.5'
+#
+#print(ondalikli1 + ondalikli2)
+#
+#ondalikli3 = float(ondalikli3)
+#print(ondalikli1 + ondalikli3)
+
+
+
+#sayi1 = float(input('Birinci değeri giriniz: '))
+#sayi2 = float(input('İkinci değeri giriniz : '))
+#
+#toplam = sayi1 + sayi2
+#print('Toplam = ', toplam)
+
+#
+#yas = int(input("Yaşını gir: "))
+#
+#if yas > 18 and yas <= 40:
+#    print("Aile izni gerekmiyor.")
+#elif yas > 0 and yas < 18:
+#    print("Giremezsin")
+#elif yas < 0:
+#    print("Böyle bir yaş yok!")
+#elif yas > 40:
+#    print("Yaşın çok büyük.")
+#else:
+#    print("Aile izni gerekir.")
+
+
+
+
+#sayi1 = int(input("Birinci sayı : "))
+#sayi2 = int(input("İkinci sayı  : "))
+
+# sayi1 büyük sayi2 olabilir
+# sayi1 küçük sayi2 olabilir
+# sayi1 eşit sayi2 olabilir
+
+#if sayi1 > sayi2:
+#    print("Büyük sayı: ", sayi1)
+#elif sayi2 > sayi1:
+#    print("Büyük sayı: ", sayi2)
+#else:
+#    print("sayi1 eşittir sayi2'ye")
+
+
+#sayi1 = int(input("Sayi1 gir: "))
+#sayi2 = int(input("Sayi2 gir: "))
+#
+#ortalama = (sayi1 + sayi2) / 2
+#print(ortalama)
+
+
+# -------------------------------
+
+#   Bir yılda 12 ay vardır. Hangisi 30,
+# hangisi 31 ve hangisi 28 çeker?
+
+
+#ay = int(input("Bir ay numarası giriniz: "))
+#
+#if ay == 1 or ay == 3 or ay == 5 or ay == 7 or ay == 8 or ay == 10 or ay == 12:
+#    print("Bu ay 31 çeker.")
+#elif ay == 4 or ay == 6 or ay == 9 or ay == 11:
+#    print("Bu ay 30 çeker.")
+#elif ay == 2:
+#    print("Bu ay 28 çeker.")
+#else:
+#    print("Böyle bir ay numarası yok!")
+
+
+
+
+
+
+
